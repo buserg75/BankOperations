4312504e0ba44c0dce093f1042c94abe5ff94ee6
Sergei <buchserg@gmail.com>
2025-04-13 00:51:19 +0300

    первая функция готова

---
+++ src/widget.py
@@ -0,0 +1,17 @@
+from masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(type_numbers: str) -> str:
+    """Функция, которая принимает на вход строку, содержащую тип и номер карты или счета,
+    и возвращает строку с замаскированным номером"""
+    type_numbers_list = type_numbers.split()
+    type = []
+    numbers = ''
+    for i in type_numbers_list:
+        if 'Счет' in i:
+            return f"Счет {get_mask_account(type_numbers_list[1])}"
+        elif i.isalpha():
+            type.append(i)
+        else:
+            numbers += i
+            return f"{' '.join(type)} {get_mask_card_number(numbers)}"
+
