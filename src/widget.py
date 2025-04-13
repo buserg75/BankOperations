dc3bbb3df4e973930cbc954e96a30ac7736d544f
Sergei <buchserg@gmail.com>
2025-04-14 00:12:49 +0300

    сделаны поправки в коде!

--- src/widget.py
+++ src/widget.py
@@ -1,24 +1,19 @@
-from masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(type_numbers: str) -> str:
     """Функция, которая принимает на вход строку, содержащую тип и номер карты или счета,
     и возвращает строку с замаскированным номером"""
-    type_numbers_list = type_numbers.split()
-    type = []
-    numbers = ''
-    for i in type_numbers_list:
-        if 'Счет' in i:
-            return f"Счет {get_mask_account(type_numbers_list[1])}"
-        elif i.isalpha():
-            type.append(i)
-        else:
-            numbers += i
-            return f"{' '.join(type)} {get_mask_card_number(numbers)}"
+    account_name, account_number = type_numbers.rsplit(' ', maxsplit=1)
+    if account_name == 'Счет':
+        masked_account_number = get_mask_account(account_number)
+    else:
+        masked_account_number = get_mask_card_number(account_number)
+    return f'{account_name} {masked_account_number}'
 
 
 def get_date(date_string: str) -> str:
     """Функция, которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
-    date_list = date_string[:10].split('-')[::-1]
-    return '.'.join(date_list)
-
+    date_list = date_string[:10].split("-")[::-1]
+    return ".".join(date_list)
