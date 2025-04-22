2b8d9cc458e6aca2fb43acacdfbeb08c86d1f067
Sergei <buchserg@gmail.com>
2025-04-22 22:07:57 +0300

    added first func filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,7 @@
+from typing import List, Dict, Any
+
+def filter_by_state(data_list: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
+    """Функция возвращает новый список, отфильтрованный по указанному значению"""
+    return [i for i in data_list if i.get("state") == state]
+
+
